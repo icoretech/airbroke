af8eeceb65d91fcf5a63a0a4e83981910ba990b2
Claudio Poli <masterkain@gmail.com>
2023-05-14 22:46:01 +0200

    add OPTIONS and cors support with a `*` fallback, not yet added to the readme because maybe it's an info that we can store in projects

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/v3/projects/:project_id/create-notice',
+        destination: '/api/v3/projects/:project_id/notices',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
