b41677f63109ca7bd348ad6feb7f776825fa2484
Claudio Poli <masterkain@gmail.com>
2023-05-16 05:51:50 +0200

    test build without tremor

--- components/OccurrenceCard.tsx
+++ components/OccurrenceCard.tsx
@@ -1,57 +1,24 @@
-'use client';
-
+import { BacktraceItem, OccurrenceTabKeys } from '@/types/airbroke';
 import { Prisma, notice, occurrence, project } from '@prisma/client';
-import { Flex, List, ListItem, Tab, TabList, Text, Title } from '@tremor/react';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState, useTransition } from 'react';
-import { FaRegAddressBook } from 'react-icons/fa';
-import { HiCubeTransparent } from 'react-icons/hi';
-import { MdOutlineWebhook } from 'react-icons/md';
-import { SiCodefactor } from 'react-icons/si';
-import { TbCubeUnfolded } from 'react-icons/tb';
+import Link from 'next/link';
 import LinkedBacktraceLine from './LinkedBacktraceLine';
 
-interface BacktraceItem {
-  file: string;
-  line: number;
-  function: string;
-}
 interface KeyValuePair {
   key: string;
   value: any;
 }
-const TAB_INDEX_MAP = {
-  backtrace: 0,
-  context: 1,
-  environment: 2,
-  session: 3,
-  params: 4,
-} as const;
+
 export default function OccurrenceCard({
   project,
   notice,
   occurrence,
+  currentTab = 'backtrace',
 }: {
   project: project;
   notice: notice;
   occurrence: occurrence;
+  currentTab?: OccurrenceTabKeys;
 }) {
-  const [selectedTab, setSelectedTab] = useState<keyof typeof TAB_INDEX_MAP>('backtrace' as const);
-  const { replace } = useRouter();
-  const [isPending, startTransition] = useTransition();
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-
-  useEffect(() => {
-    const tabParam = searchParams.get('tab');
-
-    if (tabParam && tabParam in TAB_INDEX_MAP) {
-      setSelectedTab(tabParam as keyof typeof TAB_INDEX_MAP);
-    } else {
-      setSelectedTab('backtrace'); // Set a default tab if no tab is specified
-    }
-  }, [searchParams]);
-
   function isBacktraceItem(item: any): item is BacktraceItem {
     return item && typeof item.file === 'string' && typeof item.line === 'number' && typeof item.function === 'string';
   }
@@ -67,30 +34,43 @@ export default function OccurrenceCard({
     );
   }
 
-  const setUrlForTab = (tab: string) => {
-    setSelectedTab(tab as keyof typeof TAB_INDEX_MAP);
-    const params = new URLSearchParams(searchParams.toString());
-
-    if (tab) {
-      params.set('tab', tab);
-    } else {
-      params.delete('tab');
-    }
-
-    startTransition(() => replace(`${pathname}?${params.toString()}`));
-  };
+  const tabs = [
+    { id: 'backtrace', name: 'Backtrace', current: currentTab === 'backtrace' },
+    { id: 'context', name: 'Context', current: currentTab === 'context' },
+    { id: 'environment', name: 'Environment', current: currentTab === 'environment' },
+    { id: 'session', name: 'Session', current: currentTab === 'session' },
+    { id: 'params', name: 'Params', current: currentTab === 'params' },
+  ].map((tab) => ({
+    ...tab,
+    href: `/projects/${project.id}/notices/${notice.id}/occurrences/${occurrence.id}?tab=${tab.id}`,
+  }));
 
   return (
     <div className="px-4 sm:px-6 lg:px-8">
-      <TabList value={selectedTab} onValueChange={setUrlForTab} className="mt-6">
+      <nav className="flex overflow-x-auto border-b border-white/10 py-4">
+        <ul
+          role="list"
+          className="flex min-w-full flex-none gap-x-6 px-4 text-sm font-semibold leading-6 text-gray-400 sm:px-6 lg:px-8"
+        >
+          {tabs.map((item) => (
+            <li key={item.name}>
+              <Link href={item.href} className={item.current ? 'text-indigo-400' : ''}>
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      {/* <TabList value={selectedTab} onValueChange={setUrlForTab} className="mt-6">
         <Tab value="backtrace" text="Backtrace" icon={SiCodefactor} />
         <Tab value="context" text="Context" icon={HiCubeTransparent} />
         <Tab value="environment" text="Environment" icon={TbCubeUnfolded} />
         <Tab value="session" text="Session" icon={FaRegAddressBook} />
         <Tab value="params" text="Params" icon={MdOutlineWebhook} />
-      </TabList>
+      </TabList> */}
 
-      {selectedTab === 'backtrace' &&
+      {currentTab === 'backtrace' &&
         occurrence.backtrace &&
         typeof occurrence.backtrace === 'object' &&
         Array.isArray(occurrence.backtrace) && (
@@ -98,56 +78,50 @@ export default function OccurrenceCard({
             {(occurrence.backtrace as Prisma.JsonArray).map(
               (trace, index) =>
                 isBacktraceItem(trace) && (
-                  <Flex
-                    key={index}
-                    flexDirection="row"
-                    justifyContent="start"
-                    alignItems="start"
-                    className="flex-wrap font-mono text-xs"
-                  >
-                    <Text
+                  <div key={index} className="flex  flex-row flex-wrap justify-start font-mono text-xs">
+                    <p
                       className={`text-xs text-slate-600 ${trace.file.includes('PROJECT_ROOT') ? 'font-semibold' : ''}`}
                     >
                       <LinkedBacktraceLine file={trace.file} line={trace.line} project={project} />
-                    </Text>
-                    <Text className="mx-1">:</Text>
-                    <Text className="text-xs font-semibold text-fuchsia-800">{trace.line}</Text>
-                    <Text className="mx-1">→</Text>
-                    <Text className="text-xs font-semibold text-red-600">{trace.function}</Text>
-                  </Flex>
+                    </p>
+                    <p className="mx-1">:</p>
+                    <p className="text-xs font-semibold text-fuchsia-800">{trace.line}</p>
+                    <p className="mx-1">→</p>
+                    <p className="text-xs font-semibold text-red-600">{trace.function}</p>
+                  </div>
                 )
             )}
           </div>
         )}
 
-      {selectedTab === 'context' && (
+      {currentTab === 'context' && (
         <div className="mt-6">
           {isObjectWithKeys(occurrence.context) && (
-            <List>
+            <ul>
               {objectToArray(occurrence.context).map((item: KeyValuePair) => (
-                <ListItem key={item.key}>
+                <li key={item.key}>
                   <span className="font-semibold">{item.key}:</span>
                   <span>{JSON.stringify(item.value)}</span>
-                </ListItem>
+                </li>
               ))}
-            </List>
+            </ul>
           )}
         </div>
       )}
 
-      {selectedTab === 'environment' && (
+      {currentTab === 'environment' && (
         <div className="mt-5">
-          <Title>{JSON.stringify(occurrence.environment)}</Title>
+          <h1>{JSON.stringify(occurrence.environment)}</h1>
         </div>
       )}
-      {selectedTab === 'session' && (
+      {currentTab === 'session' && (
         <div className="mt-5">
-          <Title>{JSON.stringify(occurrence.session)}</Title>
+          <h1>{JSON.stringify(occurrence.session)}</h1>
         </div>
       )}
-      {selectedTab === 'params' && (
+      {currentTab === 'params' && (
         <div className="mt-5">
-          <Title>{JSON.stringify(occurrence.params)}</Title>
+          <h1>{JSON.stringify(occurrence.params)}</h1>
         </div>
       )}
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,7 @@ const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
