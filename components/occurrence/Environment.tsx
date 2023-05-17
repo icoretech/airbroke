1ce5e68b35ec63efb0ed32fc508b2ffcc6d5fb09
Claudio Poli <masterkain@gmail.com>
2023-05-17 21:41:12 +0200

    feat(occurrence): add occurrenceUtils module with flattenObject function to flatten nested objects into an array of key-value pairs refactor(occurrence): use flattenObject function from occurrenceUtils module to flatten context, environment, params and session objects in their respective components

--- components/occurrence/Environment.tsx
+++ components/occurrence/Environment.tsx
@@ -1,27 +1,13 @@
+import { KeyValuePair, flattenObject, isObjectWithKeys } from '@/lib/occurrenceUtils';
 import { occurrence } from '@prisma/client';
-interface KeyValuePair {
-  key: string;
-  value: any;
-}
 
-function objectToArray(obj: Record<string, any>): KeyValuePair[] {
-  return Object.entries(obj).map(([key, value]) => ({ key, value }));
-}
-function isObjectWithKeys(item: any): item is Record<string, any> {
-  return (
-    item &&
-    typeof item === 'object' &&
-    !Array.isArray(item) &&
-    Object.keys(item).every((key) => typeof key === 'string')
-  );
-}
 export default function Environment({ occurrence }: { occurrence: occurrence }) {
   return (
     <div className="px-4 text-white sm:px-6 lg:px-8">
       <div className="mt-6 border-t border-white/10">
         {isObjectWithKeys(occurrence.environment) && (
           <dl className="divide-y divide-white/10">
-            {objectToArray(occurrence.environment).map((item: KeyValuePair) => (
+            {flattenObject(occurrence.environment).map((item: KeyValuePair) => (
               <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0" key={item.key}>
                 <dt className="text-sm font-medium leading-6 text-white">{item.key}</dt>
                 <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">
---
+++ lib/occurrenceUtils.ts
@@ -0,0 +1,30 @@
+export interface KeyValuePair {
+  key: string;
+  value: any;
+}
+
+export function isObjectWithKeys(item: any): item is Record<string, any> {
+  return (
+    item &&
+    typeof item === 'object' &&
+    !Array.isArray(item) &&
+    Object.keys(item).every((key) => typeof key === 'string')
+  );
+}
+
+export function flattenObject(obj: Record<string, any> | null | undefined, prefix = ''): KeyValuePair[] {
+  if (obj == null) {
+    return [];
+  }
+
+  return Object.entries(obj).flatMap(([key, value]) => {
+    const newKey = prefix ? `${prefix}.${key}` : key;
+    if (Array.isArray(value)) {
+      return value.flatMap((item, index) => flattenObject(item, `${newKey}[${index}]`));
+    } else if (isObjectWithKeys(value)) {
+      return flattenObject(value, newKey);
+    } else {
+      return { key: newKey, value };
+    }
+  });
+}
