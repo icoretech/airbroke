73e81c684c7add3723edd74ba3b6d55cf84947d9
Claudio Poli <masterkain@gmail.com>
2023-05-17 21:22:52 +0200

    refactor(components): refactor Context, Environment, Params, and Session components to display occurrence data in a more readable and organized way. Use a definition list to display key-value pairs of occurrence context, environment, params, and session.

--- components/occurrence/Context.tsx
+++ components/occurrence/Context.tsx
@@ -19,17 +19,21 @@ function isObjectWithKeys(item: any): item is Record<string, any> {
 
 export default function Context({ occurrence }: { occurrence: occurrence }) {
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
-      {isObjectWithKeys(occurrence.context) && (
-        <ul>
-          {objectToArray(occurrence.context).map((item: KeyValuePair) => (
-            <li key={item.key}>
-              <span className="font-semibold">{item.key}:</span>
-              <span>{JSON.stringify(item.value)}</span>
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className="px-4 text-white sm:px-6 lg:px-8">
+      <div className="mt-6 border-t border-white/10">
+        {isObjectWithKeys(occurrence.context) && (
+          <dl className="divide-y divide-white/10">
+            {objectToArray(occurrence.context).map((item: KeyValuePair) => (
+              <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0" key={item.key}>
+                <dt className="text-sm font-medium leading-6 text-white">{item.key}</dt>
+                <dd className="mt-1 text-sm leading-6 text-gray-400 sm:col-span-2 sm:mt-0">
+                  {JSON.stringify(item.value)}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        )}
+      </div>
     </div>
   );
 }
