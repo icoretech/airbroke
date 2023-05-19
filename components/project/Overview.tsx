90657348cf1d05460014d3fbe03b638c78559801
Claudio Poli <masterkain@gmail.com>
2023-05-19 19:18:51 +0200

    feat(project): add Overview component to display project overview information refactor(project): rename 'Activity' tab to 'Integrations' to better reflect its content refactor(CodeTemplate): increase z-index to prevent overlapping with other elements fix(ConfirmationDialog): increase z-index to prevent overlapping with other elements

---
+++ components/project/Overview.tsx
@@ -0,0 +1,5 @@
+import { project } from '@prisma/client';
+
+export default function Overview({ project }: { project: project }) {
+  return <div className="px-4 text-white sm:px-6 lg:px-8"></div>;
+}
