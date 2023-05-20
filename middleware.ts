1948bc4d4dc0b383a80f49fe5b0915c0c2f2b228
Claudio Poli <masterkain@gmail.com>
2023-05-20 16:43:20 +0200

    chore: add robots.txt to the list of excluded paths in middleware to avoid unnecessary logging of requests to it

--- middleware.ts
+++ middleware.ts
@@ -8,8 +8,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - robots.txt (robots file)
      * - the homepage ("/")
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).+)',
+    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt).+)',
   ],
 };
