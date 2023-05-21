e6e73ef71dacac59f74830073a70f1ac17778756
Claudio Poli <masterkain@gmail.com>
2023-05-21 23:26:53 +0200

    add app icon

--- components/occurrence/toolbox/AI.tsx
+++ components/occurrence/toolbox/AI.tsx
@@ -47,7 +47,7 @@ export default function ToolboxAI({ occurrenceId }: { occurrenceId: bigint }) {
         <textarea
           readOnly
           value={data}
-          className="h-60 w-full resize-none rounded-lg bg-gray-700 px-4 py-2 text-xs text-white"
+          className="h-60 w-full resize-none rounded-lg bg-gray-700 px-4 py-2 text-xs text-white  scrollbar-none"
         ></textarea>
       </div>
       <div>
