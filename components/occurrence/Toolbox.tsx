1da95f881d060f0e913fdf105a4c4b34aad7eab0
Claudio Poli <masterkain@gmail.com>
2023-05-22 05:01:45 +0200

    style(Toolbox.tsx): add xl and 2xl breakpoints to the toolbox grid to improve responsiveness and layout consistency, refs https://github.com/icoretech/airbroke/issues/24

--- components/occurrence/Toolbox.tsx
+++ components/occurrence/Toolbox.tsx
@@ -6,7 +6,10 @@ import ToolboxFetch from './toolbox/Replay';
 export default function Toolbox({ occurrence }: { occurrence: occurrence }) {
   return (
     <div className="px-4 sm:px-6 lg:px-8">
-      <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <ul
+        role="list"
+        className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4"
+      >
         <li className="col-span-1 flex flex-col divide-y divide-indigo-400/30 rounded-lg text-center shadow">
           <ToolboxAI occurrenceId={occurrence.id} />
         </li>
