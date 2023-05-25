4836b382deb289dc66e4d1a4af1e71e26c168076
Claudio Poli <masterkain@gmail.com>
2023-05-25 07:26:40 +0200

    feat(ProjectsTable.tsx): add hover effect to project list items to improve user experience
    refactor(ProjectsTable.tsx): rename statuses object to statusClasses for better semantics
    
    refactor(ProjectsTable.tsx): remove unused ProviderIcon component and related code
    
    refactor(ProjectsTable.tsx): remove repository information from project list item to simplify UI and improve readability

--- components/ProjectsTable.tsx
+++ components/ProjectsTable.tsx
@@ -1,48 +1,49 @@
 import classNames from '@/lib/classNames';
-import { composeRepoUrl } from '@/lib/gitProvider';
 import { project } from '@prisma/client';
 import Link from 'next/link';
 import OccurrenceCounterLabel from './CounterLabel';
-import { ProviderIcon } from './ProviderIcon';
+
+const statusClasses = {
+  dark: 'text-gray-500 bg-gray-100/50',
+  green: 'text-green-400 bg-green-400/50',
+  red: 'text-rose-400 bg-rose-400/50',
+};
 
 export default function ProjectsTable({ projects }: { projects: project[] }) {
-  const statuses = {
-    offline: 'text-gray-500 bg-gray-100/50',
-    online: 'text-green-400 bg-green-400/50',
-    error: 'text-rose-400 bg-rose-400/50',
-  };
-
   return (
     <ul role="list" className="divide-y divide-white/5">
       {projects.map((project) => (
-        <li key={project.id.toString()} className="relative flex items-center space-x-4 px-4 py-4 sm:px-6 lg:px-8">
+        <li
+          key={project.id.toString()}
+          className="relative flex items-center space-x-4 px-4 py-4 transition-colors duration-200 hover:bg-airbroke-800 sm:px-6 lg:px-8"
+        >
           <div className="min-w-0 flex-auto">
             <div className="flex items-center gap-x-3">
               <span className="relative flex h-2 w-2">
                 <span
                   className={classNames(
-                    statuses[project.notices_count === BigInt(0) ? 'online' : 'error'],
+                    statusClasses[project.notices_count === BigInt(0) ? 'green' : 'red'],
                     'absolute inline-flex h-full w-full rounded-full',
                     project.notices_count !== BigInt(0) && 'animate-ping'
                   )}
                 ></span>
                 <span
                   className={classNames(
-                    statuses[project.notices_count === BigInt(0) ? 'online' : 'error'],
+                    statusClasses[project.notices_count === BigInt(0) ? 'green' : 'red'],
                     'relative inline-flex h-2 w-2 rounded-full'
                   )}
                 ></span>
               </span>
 
               <h2 className="relative min-w-0 text-sm font-semibold leading-6 text-white">
-                <Link href={`/projects/${project.id.toString()}/notices`} className="flex gap-x-2">
+                <Link href={`/projects/${project.id}/notices`} className="flex gap-x-2">
                   <span className="truncate">{project.organization.toLowerCase()}</span>
                   <span className="text-gray-400">/</span>
                   <span className="cursor-pointer whitespace-nowrap">{project.name}</span>
                   <span className="absolute inset-0" />
                 </Link>
                 <Link
-                  href={`/projects/${project.id.toString()}/notices`}
+                  href={`/projects/${project.id}/notices`}
                   className={classNames(
                     'absolute inset-0 flex gap-x-2 bg-gradient-to-r from-gray-400 bg-clip-text text-transparent opacity-0 transition-opacity duration-300 ease-in-out hover:opacity-100',
                     project.notices_count === BigInt(0) ? 'to-green-600' : 'to-red-600'
@@ -54,25 +55,9 @@ export default function ProjectsTable({ projects }: { projects: project[] }) {
                 </Link>
               </h2>
             </div>
-            <div className="ml-5 mt-1 flex items-center gap-x-2.5 text-xs leading-5 text-gray-400">
-              <p className="inline-flex items-center whitespace-nowrap">
-                <ProviderIcon
-                  provider={project.repo_provider}
-                  className="h-1em w-1em mr-1 shrink-0"
-                  aria-hidden="true"
-                />
-                <Link href={composeRepoUrl(project)} className="hover:text-white">
-                  {project.organization.toLowerCase()} / {project.name.toLowerCase()}
-                </Link>
-              </p>
-              <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 flex-none fill-gray-300">
-                <circle cx={1} cy={1} r={1} />
-              </svg>
-              <p className="truncate">{project.repo_branch}</p>
-            </div>
           </div>
 
-          <Link href={`/projects/${project.id.toString()}/notices`}>
+          <Link href={`/projects/${project.id}/notices`}>
             <OccurrenceCounterLabel counter={project.notices_count} />
           </Link>
         </li>
