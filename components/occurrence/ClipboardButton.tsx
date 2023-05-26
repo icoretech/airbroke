d6f53369512d88131bf56cf126a7409cfc77931a
Claudio Poli <masterkain@gmail.com>
2023-05-26 05:38:54 +0200

    fix(page.tsx): remove unused project_id parameter from Notice component chore(Backtrace.tsx): comment out BookmarkButton import and usage style(ClipboardButton.tsx): update button styles to match design system and add hover effect

--- components/occurrence/ClipboardButton.tsx
+++ components/occurrence/ClipboardButton.tsx
@@ -20,7 +20,7 @@ export default function ClipboardButton({ json }: { json: Prisma.JsonValue }) {
   return (
     <button
       onClick={handleCopy}
-      className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-900 px-3 py-2 text-sm font-semibold text-indigo-200 hover:bg-indigo-800 hover:text-white"
+      className="inline-flex items-center gap-x-1.5 rounded-md px-3  py-2 text-sm font-semibold text-indigo-200 shadow-sm transition-colors duration-200 hover:bg-indigo-500 hover:text-white"
     >
       <HiClipboardCopy className="h-4 w-4 md:h-5 md:w-5" aria-hidden="true" />
       {isCopied ? 'Copied!' : 'Copy to Clipboard'}
