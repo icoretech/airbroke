f94bdc50e7fb07e4562fddbd570503604a534e51
Claudio Poli <masterkain@gmail.com>
2023-05-26 05:52:36 +0200

    refactor(page.tsx): replace Link component with normal link

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default async function HomePage() {
           <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
             <Image src={logo} alt="Airbroke logo" className="h-11 w-auto" />
             <div className="mt-24 sm:mt-32 lg:mt-16">
-              <Link href="https://github.com/icoretech/airbroke/releases" className="inline-flex space-x-6">
+              <a href="https://github.com/icoretech/airbroke/releases" className="inline-flex space-x-6">
                 <span className="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20">
                   Latest Releases
                 </span>
-              </Link>
+              </a>
             </div>
             <h1 className="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
               Hello, welcome to Airbroke
@@ -36,9 +36,9 @@ export default async function HomePage() {
                 Go to Projects
               </Link>
 
-              <Link href="https://github.com/icoretech/airbroke" className="text-sm font-semibold leading-6 text-white">
+              <a href="https://github.com/icoretech/airbroke" className="text-sm font-semibold leading-6 text-white">
                 Learn more <span aria-hidden="true">→</span>
-              </Link>
+              </a>
             </div>
           </div>
           <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
@@ -55,10 +55,10 @@ export default async function HomePage() {
       <footer className="bg-gray-900">
         <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
           <div className="flex justify-center space-x-6 md:order-2">
-            <Link href="https://github.com/icoretech/airbroke" className="text-white hover:text-gray-500">
+            <a href="https://github.com/icoretech/airbroke" className="text-white hover:text-gray-500">
               <span className="sr-only">github</span>
               <FaGithub className="h-6 w-6" aria-hidden="true" />
-            </Link>
+            </a>
           </div>
           <div className="mt-8 md:order-1 md:mt-0">
             <p className="text-center text-xs leading-5 text-white">&copy; {currentYear} iCoreTech, Inc.</p>
