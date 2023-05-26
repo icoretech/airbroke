8cc5159411925051ade73788ba681ef0bcca73fd
Claudio Poli <masterkain@gmail.com>
2023-05-26 22:48:03 +0200

    refactor(OccurrenceChart.tsx): replace @nivo/bar with react-chartjs-2 feat(OccurrenceChart.tsx): convert data to Chart.js format and add options chore(package.json): remove @nivo/bar and add chart.js and react-chartjs-2 dependencies

--- components/OccurrenceChart.tsx
+++ components/OccurrenceChart.tsx
@@ -1,77 +1,67 @@
 'use client';
-import dynamic from 'next/dynamic';
 
-// Define the interface for the 'data' prop.
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  ChartOptions,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from 'chart.js';
+import { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
 interface DataItem {
   date: string;
   count: number;
 }
 
-interface OccurrenceChartProps {
-  data: DataItem[];
+interface ChartDataType {
+  labels: string[];
+  datasets: {
+    label: string;
+    data: number[];
+    backgroundColor: string;
+    borderColor: string;
+    borderWidth: number;
+  }[];
 }
-const ResponsiveBar: any = dynamic(() => import('@nivo/bar').then((m) => m.ResponsiveBar), { ssr: false });
 
-export default function OccurrenceChart({ data }: OccurrenceChartProps) {
-  return (
-    <div style={{ height: '50vh' }}>
-      <ResponsiveBar
-        data={data}
-        keys={['count']}
-        indexBy="date"
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-        padding={0.3}
-        valueScale={{ type: 'linear' }}
-        indexScale={{ type: 'band', round: true }}
-        colors={['#9cbbf2']} // Custom color for the bars, adjust as needed
-        borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          tickValues: [], // hides all tick values
-          legendPosition: 'middle',
-          legendOffset: 32,
-          tickTextColor: '#FFFFFF', // white color for tick text
-        }}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legendPosition: 'middle',
-          legendOffset: -40,
-          legendTextColor: '#FFFFFF', // white color for legend text
-          tickTextColor: '#FFFFFF', // white color for tick text
-        }}
-        theme={{
-          axis: {
-            ticks: {
-              line: {
-                stroke: '#192231', // Custom color for axis lines
-              },
-              text: {
-                fill: '#FFFFFF', // white color for the axis labels
-              },
-            },
-            domain: {
-              line: {
-                stroke: '#192231', // Custom color for domain line
-              },
-            },
+export default function OccurrenceChart({ data }: { data: { date: string; count: number }[] }) {
+  const [chartData, setChartData] = useState<ChartDataType | null>(null);
+
+  useEffect(() => {
+    if (data) {
+      setChartData({
+        labels: data.map((item) => item.date),
+        datasets: [
+          {
+            label: 'Occurrences',
+            data: data.map((item) => item.count),
+            backgroundColor: 'rgba(86, 116, 185, 0.8)',
+            borderColor: 'rgba(86, 116, 185, 1)',
+            borderWidth: 1,
           },
-          grid: {
-            line: {
-              stroke: '#192231', // Custom color for grid lines
-            },
-          },
-        }}
-        enableLabel={false}
-        animate={true}
-        motionConfig="gentle"
-        legends={[]}
-      />
-    </div>
-  );
+        ],
+      });
+    }
+  }, [data]);
+
+  const options: ChartOptions<'bar'> = {
+    responsive: true,
+    scales: {
+      x: {
+        type: 'category',
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return chartData && <Bar data={chartData} options={options} />;
 }
