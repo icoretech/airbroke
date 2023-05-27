3fbd15c954a4e522224a91a29cd13f8b9bd20e3c
Claudio Poli <masterkain@gmail.com>
2023-05-27 10:32:19 +0200

    feat(occurrence page): replace prisma query with getOccurrenceById function in occurrence page and related components to improve code semantics and readability

--- components/occurrence/Toolbox.tsx
+++ components/occurrence/Toolbox.tsx
@@ -1,10 +1,15 @@
+import { getOccurrenceById } from '@/lib/queries/occurrences';
 import { Context } from '@/types/airbroke';
-import { Occurrence } from '@prisma/client';
 import ToolboxAI from './toolbox/AI';
 import ToolboxCurl from './toolbox/Curl';
 import ToolboxFetch from './toolbox/Replay';
 
-export default function Toolbox({ occurrence }: { occurrence: Occurrence }) {
+export default async function Toolbox({ occurrenceId }: { occurrenceId: string }) {
+  const occurrence = await getOccurrenceById(occurrenceId);
+  if (!occurrence) {
+    throw new Error('Occurrence not found');
+  }
+
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
