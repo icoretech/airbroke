46e7596be53644cec1ddae1fc1b15e43c1116c7c
Claudio Poli <masterkain@gmail.com>
2023-05-28 18:18:53 +0200

    fix(occurrenceUtils.ts): flattenObject function now returns single string values in arrays as is to improve readability of occurrence data

--- lib/occurrenceUtils.ts
+++ lib/occurrenceUtils.ts
@@ -20,7 +20,12 @@ export function flattenObject(obj: Record<string, any> | null | undefined, prefi
   return Object.entries(obj).flatMap(([key, value]) => {
     const newKey = prefix ? `${prefix}.${key}` : key;
     if (Array.isArray(value)) {
-      return value.flatMap((item, index) => flattenObject(item, `${newKey}[${index}]`));
+      // If the array has only one value and it's a string, return it as is
+      if (value.length === 1 && typeof value[0] === 'string') {
+        return { key: newKey, value: value[0] };
+      } else {
+        return value.flatMap((item, index) => flattenObject(item, `${newKey}[${index}]`));
+      }
     } else if (isObjectWithKeys(value)) {
       return flattenObject(value, newKey);
     } else {
