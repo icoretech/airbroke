ce72849755b33cd13e18b105a581765e91c5f484
Claudio Poli <masterkain@gmail.com>
2023-05-28 09:06:31 +0200

    style(NoData.tsx): refactor Airbrake buttons

--- components/NoData.tsx
+++ components/NoData.tsx
@@ -35,36 +35,53 @@ export default function NoData({ project, showHeader = true }: { project: Projec
         </div>
       )}
 
-      <div className="grid grid-cols-1 justify-items-center gap-4">
-        <div className="rounded-lg bg-gray-900 p-6 shadow-lg">
-          <h2 className="mb-2 text-lg font-bold text-indigo-500">Airbrake JS</h2>
-          <button
-            onClick={() => startTransition(() => sendAirbrakeJsException())}
-            disabled={isPending}
-            className="inline-flex w-full items-center justify-center rounded-md bg-indigo-400/10 px-3 py-2 text-sm font-semibold text-indigo-400 shadow-sm ring-indigo-400/30 transition-colors duration-200 hover:bg-indigo-500 hover:text-indigo-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 disabled:text-white"
-          >
-            {isPending ? (
-              <SlDisc className="-ml-0.5 mr-1.5 h-5 w-5 animate-spin" aria-hidden="true" />
-            ) : (
-              <SlEnergy className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-            )}
-            <span>{isPending ? 'Sending...' : 'Send a test exception'}</span>
-          </button>
+      <div className="mb-5">
+        <div className="rounded-md bg-gray-800 p-4 sm:flex sm:items-start sm:justify-between">
+          <div className="sm:flex sm:flex-col sm:items-start">
+            <div className="text-sm font-bold leading-6 text-gray-300">Airbrake</div>
+            <div className="mt-1 text-sm text-gray-300">JavaScript</div>
+          </div>
+          <div className="mt-4 flex items-center justify-center sm:ml-6 sm:mt-0 sm:flex-shrink-0">
+            <div className="flex items-center justify-center">
+              <button
+                onClick={() => startTransition(() => sendAirbrakeJsException())}
+                disabled={isPending}
+                className="inline-flex w-full items-center justify-center rounded-md bg-indigo-400/10 px-3 py-2 text-sm font-semibold text-indigo-400 shadow-sm ring-indigo-400/30 transition-colors duration-200 hover:bg-indigo-500 hover:text-indigo-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 disabled:text-white"
+              >
+                {isPending ? (
+                  <SlDisc className="-ml-0.5 mr-1.5 h-5 w-5 animate-spin" aria-hidden="true" />
+                ) : (
+                  <SlEnergy className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+                )}
+                <span className="ml-1">{isPending ? 'Sending...' : 'Test'}</span>
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="rounded-lg bg-gray-900 p-6 shadow-lg">
-          <h2 className="mb-2 text-lg font-bold text-indigo-500">Airbrake Node</h2>
-          <button
-            onClick={() => startTransition(() => sendAirbrakeNodeException(project.id, window.location.origin))}
-            disabled={isPending}
-            className="inline-flex w-full items-center justify-center rounded-md bg-indigo-400/10 px-3 py-2 text-sm font-semibold text-indigo-400 shadow-sm ring-indigo-400/30 transition-colors duration-200 hover:bg-indigo-500 hover:text-indigo-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 disabled:text-white"
-          >
-            {isPending ? (
-              <SlDisc className="-ml-0.5 mr-1.5 h-5 w-5 animate-spin" aria-hidden="true" />
-            ) : (
-              <SlEnergy className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-            )}
-            <span>{isPending ? 'Sending...' : 'Send a test exception'}</span>
-          </button>
+      </div>
+
+      <div className="mb-5">
+        <div className="rounded-md bg-gray-800 p-4 sm:flex sm:items-start sm:justify-between">
+          <div className="sm:flex sm:flex-col sm:items-start">
+            <div className="text-sm font-bold leading-6 text-gray-300">Airbrake</div>
+            <div className="mt-1 text-sm text-gray-300">Node.js</div>
+          </div>
+          <div className="mt-4 flex items-center justify-center sm:ml-6 sm:mt-0 sm:flex-shrink-0">
+            <div className="flex items-center justify-center">
+              <button
+                onClick={() => startTransition(() => sendAirbrakeNodeException(project.id, window.location.origin))}
+                disabled={isPending}
+                className="inline-flex w-full items-center justify-center rounded-md bg-indigo-400/10 px-3 py-2 text-sm font-semibold text-indigo-400 shadow-sm ring-indigo-400/30 transition-colors duration-200 hover:bg-indigo-500 hover:text-indigo-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 disabled:text-white"
+              >
+                {isPending ? (
+                  <SlDisc className="-ml-0.5 mr-1.5 h-5 w-5 animate-spin" aria-hidden="true" />
+                ) : (
+                  <SlEnergy className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+                )}
+                <span className="ml-1">{isPending ? 'Sending...' : 'Test'}</span>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
