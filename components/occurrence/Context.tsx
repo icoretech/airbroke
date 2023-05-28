1711f4c5fc112c6183a4ea5972e463227047596c
Claudio Poli <masterkain@gmail.com>
2023-05-28 17:55:11 +0200

    fix(Context.tsx): add missing space between flex-shrink-0 and font-semibold classes in div element

--- components/occurrence/Context.tsx
+++ components/occurrence/Context.tsx
@@ -20,7 +20,7 @@ async function Context({ occurrenceId }: ContextProps) {
               className="flex items-start space-x-2 rounded border border-airbroke-800 bg-gradient-to-r from-gray-900 to-airbroke-900 p-2 shadow-md"
               key={item.key}
             >
-              <div className="flex-shrink-0font-semibold text-indigo-200">{item.key}:</div>
+              <div className="flex-shrink-0 font-semibold text-indigo-200">{item.key}:</div>
               <div className="flex-grow break-all rounded px-2 font-mono  text-gray-300">
                 {JSON.stringify(item.value)}
               </div>
