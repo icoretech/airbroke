a513e31921133efa5316be1bb02a6f8150063007
Claudio Poli <masterkain@gmail.com>
2023-07-08 00:26:20 +0200

    feat(db.ts): refactor prisma initialization

--- lib/db.ts
+++ lib/db.ts
@@ -1,23 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient({
-    log: ['warn'], errorFormat: 'pretty'
-  })
-} else {
-  const globalForPrisma = global as unknown as {
-    prisma: PrismaClient | undefined
-  }
-
-  if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = new PrismaClient({
-      log: ['warn'], errorFormat: 'pretty'
-    })
-  }
-
-  prisma = globalForPrisma.prisma
+declare global {
+  var prisma: PrismaClient | undefined;
 }
 
-export { prisma }
+const prisma = global.prisma || new PrismaClient({ log: ['warn'], errorFormat: 'pretty' });
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
+
+export default prisma;
