7bd6e6dc31cd6d6d499200171dc8b377768d9f04
Claudio Poli <masterkain@gmail.com>
2023-07-08 12:41:20 +0200

    fix(db.ts): update PrismaClient initialization to conditionally log queries and warnings based on NODE_ENV environment variable

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,12 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient({ log: ['query', 'warn'], errorFormat: 'pretty' });
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'warn'] : ['warn'],
+    errorFormat: 'pretty',
+  });
 
 if (process.env.NODE_ENV === 'development') global.prisma = prisma;
 
