c2e0d9ccbe4ba7baf7b1183c332c1bfdc9f50644
Claudio Poli <masterkain@gmail.com>
2023-07-09 14:32:26 +0200

    chore(next.config.js): fix type

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
   openAnalyzer: false,
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
