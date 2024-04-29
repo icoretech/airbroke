424e661bdba3da3d3815f08bbcd73bf92be17dd5
Claudio Poli <masterkain@gmail.com>
2024-04-30 00:29:53 +0200

    bump react

--- app/projects/new/AddButton.tsx
+++ app/projects/new/AddButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { experimental_useFormStatus as useFormStatus } from 'react-dom';
+import { useFormStatus } from 'react-dom';
 import { SlDisc } from 'react-icons-ng/sl';
 
 export default function AddButton() {
