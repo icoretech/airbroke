1d34a7791fe74a71da778c1ae4341f28cef7bfcd
kain <masterkain@gmail.com>
2026-03-20 19:05:27 +0100

    chore: serialize docker yarn bootstrap

--- docker-yarn-entrypoint.sh
+++ docker-yarn-entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 set -e
 
-# Run Yarn install to set up dependencies
-echo "Running yarn install..."
-yarn install || (echo "yarn install failed, printing build log(s):" && find /tmp -type f -name "build.log" -exec cat {} \; && exit 1)
+# Run Yarn install to set up dependencies unless a compose bootstrap service
+# already populated the shared bind mount.
+if [ "${SKIP_YARN_INSTALL:-}" != "1" ]; then
+  echo "Running yarn install..."
+  yarn install || (echo "yarn install failed, printing build log(s):" && find /tmp -type f -name "build.log" -exec cat {} \; && exit 1)
+else
+  echo "Skipping yarn install..."
+fi
 
 # Run database migrations
 echo "Running database migrations..."
